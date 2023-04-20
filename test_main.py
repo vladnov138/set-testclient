a95fc5b7d947c6c53a861797df56044913960d8a
Vankad24 <vankad24@mail.ru>
2023-04-20 13:35:30 +0800

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,31 @@
+import requests
+# url = "https://postman-echo.com/post"
+url = "http://127.0.0.1:5000"
+token = ""
+gameId = 0
+def send_request(route,**kwargs):
+	return requests.post(url+route, json = kwargs, headers={"Content-type":"application/json"}).json()
+
+def check_response(resp, **kwargs):
+	for key in kwargs:
+		assert key in resp
+		if kwargs[key] is not None:
+			assert resp[key]==kwargs[key]
+
+def test_register():
+	nick = "vanka"
+	pswd = 242424
+	response = send_request("/user/login", nickname=nick, password=pswd)
+	check_response(response, success=True, nickname=nick ,accessToken = None, error=None)
+	global token
+	token = response["accessToken"]
+
+def test_create_game():
+	response = send_request("/set/room/create", accessToken = token)
+	check_response(response, success=True, error=None, gameId=None)
+	global gameId
+	gameId = response["gameId"]
+
+# test_register()
+# test_create_game()
+
